5245f7b0d70e3a67e5f8d369d063a267c0ded782
Sohail Khan <48204572+sohailkhan24@users.noreply.github.com>
2024-09-07 11:55:02 +0530

    Initial Commit

---
+++ src/main/java/com/sohail/fitnessapp/FitnessAppApplication.java
@@ -0,0 +1,17 @@
+package com.sohail.fitnessapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Sohail
+ * Fitness Application For slot Booking
+ */
+@SpringBootApplication
+public class FitnessAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessAppApplication.class, args);
+	}
+
+}
