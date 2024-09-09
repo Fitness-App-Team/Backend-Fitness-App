2bf88c9d2ca490dcf99a9a17e16ccbaea7a06d5c
Sezal Verma <sezalverma.s@gmail.com>
2024-09-09 12:48:14 +0530

    test commit

--- src/main/java/com/sohail/fitnessapp/FitnessAppApplication.java
+++ src/main/java/com/sohail/fitnessapp/FitnessAppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author Sohail
+ * @author Sohail, Sezal
  * Fitness App For slot Booking
  *
  */
